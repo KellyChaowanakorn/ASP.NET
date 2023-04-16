d4ed9963d03fbadf8fdf8d051f6eee7e6fd322ed
Chaowanakorn rhuttanaourai <116024912+KellyChaowanakorn@users.noreply.github.com>
2023-04-16 16:34:22 +0700

    Add files via upload

---
+++ ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using BasicASPTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicASPTutorial.data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { 
+
+        }
+        public DbSet<Student>Students { get; set; }
+    }
+}
